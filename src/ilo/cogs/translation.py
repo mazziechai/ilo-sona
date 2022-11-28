ff36dfdb1ce36654c254f97b74207965e08a8a92
Gregory Danielson <gregory.danielson3@gmail.com>
2022-11-28 13:30:28 -0600

    Type-safety and env-use refactor

--- src/ilo/cogs/translation.py
+++ src/ilo/cogs/translation.py
@@ -1,34 +1,48 @@
+import logging
 from datetime import datetime
 
 import discord
 from discord import ApplicationContext
+from discord.channel import TextChannel
 from discord.ext import commands, tasks
 
-from ..bot import Ilo
-from ..db import Challenges, Sentences
-from ..ui import TranslationCogSubmitModal, TranslationVerificationView
+from ilo.bot import Ilo
+from ilo.db import Challenges, Sentences
+from ilo.ui import TranslationCogSubmitModal, TranslationVerificationView
+
+LEARNING_SERVER = 969386329513295872
+CHALLENGE_CHANNEL = 1014694309557186600
+APPROVAL_CHANNEL = 1026141020993368116
+CHALLENGER_ROLE = 1014695304605470811
+TEACHER_ROLE = 969795956264554537
+
+LOG = logging.getLogger()
 
 
 async def _post_translation_challenge(bot: Ilo):
-    guild = bot.get_guild(969386329513295872)
-    channel = guild.get_channel(1014694309557186600)
-    role = guild.get_role(1014695304605470811)
+    guild = bot.get_guild(LEARNING_SERVER)
+    assert guild
+    channel = guild.get_channel(CHALLENGE_CHANNEL)
+    assert channel and isinstance(channel, TextChannel)
+    role = guild.get_role(CHALLENGER_ROLE)
+    assert role
 
     # Since getting a random document returns a pymongo command cursor, we have to use
     # list comprehension to get the document from it, then we need to get the document id
     # specifically so we can pull the document and embed it in the challenge
     sentence_id = [i["_id"] for i in Sentences.objects(used=False, verified=True).aggregate([{"$sample": {"size": 1}}])].pop()  # type: ignore
     sentence = Sentences.objects(id=sentence_id).first()  # type: ignore
+    assert sentence
 
     challenge_number = Challenges.objects.count() + 1  # type: ignore
     challenge = Challenges(number=challenge_number, sentence=sentence).save()
 
-    msg = await channel.send(  # type: ignore
+    msg = await channel.send(
         f"__**Biweekly Translation Challenge {challenge_number}**__\n\n"
         f"{role.mention} Translate this sentence into toki pona!\n"
         f"> {sentence.sentence}\n\n"
         f"Discuss in the thread below. **Spoiler your answers!**",
-        allowed_mentions=discord.AllowedMentions(roles=[role]),  # type: ignore
+        allowed_mentions=discord.AllowedMentions(roles=[role]),
     )
     await msg.create_thread(name=f"Challenge {challenge_number}")
 
@@ -46,30 +60,38 @@ class TranslationCog(commands.Cog):
     )
     async def submit(self, ctx: ApplicationContext):
         modal = TranslationCogSubmitModal(title="Sentence submission")
+        assert ctx.user
 
         # Send the prompt for the sentence
         await ctx.send_modal(modal)
         await modal.wait()
 
         # Stripping new lines from the entry
-        sentence_content = modal.children[0].value.replace("\n", " ")
-        sentence = Sentences(user=ctx.user.id, sentence=sentence_content)
+        submission = modal.children[0].value
+        assert submission
+        submission = submission.replace("\n", " ")
+        sentence = Sentences(user=ctx.user.id, sentence=submission)
+        assert sentence
 
-        msg = await self.bot.get_channel(1026141020993368116).send(  # type: ignore
+        channel = self.bot.get_channel(APPROVAL_CHANNEL)
+        assert channel and isinstance(channel, TextChannel)
+        msg = await channel.send(
             content=f"{ctx.user.mention} li wile pana e toki ni tawa musi pi ante toki:\n"
             f"> {sentence.sentence}\n"
             "*ni li pona ala pona tawa sina?*",
-            view=TranslationVerificationView(sentence),
+            view=TranslationVerificationView(submission),
             allowed_mentions=discord.AllowedMentions.none(),
         )
+        assert msg
         sentence.verification_message = msg.id
         sentence.save()
 
-    @commands.has_role(969795956264554537)
+    @commands.has_role(TEACHER_ROLE)
     @commands.message_command(name="Approve a sentence suggestion")
     async def approve(self, ctx: ApplicationContext, message: discord.Message):
         sentence = Sentences.objects(verification_message=message.id).first()  # type: ignore
-
+        assert sentence
+        assert ctx.user
         if sentence is None:
             await ctx.respond(
                 content="That is not a sentence suggestion!", ephemeral=True
@@ -86,11 +108,12 @@ class TranslationCog(commands.Cog):
 
         await ctx.respond(content="toki ni li kama pona!", ephemeral=True)
 
-    @commands.has_role(969795956264554537)
+    @commands.has_role(TEACHER_ROLE)
     @commands.message_command(name="Deny a sentence suggestion")
     async def deny(self, ctx: ApplicationContext, message: discord.Message):
         sentence = Sentences.objects(verification_message=message.id).first()  # type: ignore
-
+        assert sentence
+        assert ctx.user
         if sentence is None:
             await ctx.respond(
                 content="That is not a sentence suggestion!", ephemeral=True
@@ -110,6 +133,8 @@ class TranslationCog(commands.Cog):
     @tasks.loop(minutes=1)
     async def post_translation_challenge(self):
         time = datetime.utcnow()
+        # race condition: bot can finish init of this cog before login
+        # if the time is right, posting will fail
         if time.weekday() == 2 or time.weekday() == 5:
             if time.hour == 18 and time.minute == 0:
                 await _post_translation_challenge(self.bot)
@@ -118,11 +143,11 @@ class TranslationCog(commands.Cog):
     async def translation_after_loop(self):
         pass
 
-    @commands.check(lambda ctx: ctx.author.id == 712104395747098656)
-    @commands.slash_command(name="debug")
-    async def debug(self, ctx: ApplicationContext):
+    @commands.is_owner()  # TODO: admins only?
+    @commands.slash_command(name="start_challenge")
+    async def start_challenge(self, ctx: ApplicationContext):
         await _post_translation_challenge(self.bot)
-        await ctx.respond(content="I hope this worked")
+        await ctx.respond(content="Sent a challenge!", ephemeral=True)
 
 
 def setup(bot: Ilo):
